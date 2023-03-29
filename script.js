3f3077f18dfd211b44fb92c7bc763f3c68f6221a
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-03-29 14:31:17 +0100

    modified sass, index htm and script

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 let btn = document.getElementById('AddJob');
 let list = document.getElementById("uljoblist");
+
+let btnAddJobs = document.getElementById("btnAddJobs");
+let divAddjob = document.getElementById("divAddjob");
+let editJob = document.getElementById("divEditJob");
+let btnSaveEdits = document.getElementById("btnSaveEdits");
+let btnCancelEdits = document.getElementById("btnCancelEdits");
+
+
+btnAddJobs.addEventListener("click",() =>{
+    divAddjobs.className="show";
+    editJob.className="hide";
+});
