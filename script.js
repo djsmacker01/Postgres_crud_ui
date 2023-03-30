e6ff895765efbb053d961848b8a3261138d125fb
Nurudeen Adedeji <djsmacker01@gmail.com>
2023-03-30 09:37:52 +0100

    modified index.html N script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (async()=>{
-let btn = document.getElementById("AddJob");
+// let btn = document.getElementById("AddJob");
 let list = document.getElementById("uljoblist");
-
+let btnNewJob = document.getElementById("btnAddNewJob");
 let btnAddJobs = document.getElementById("addJobs");
 let divAddjob = document.getElementById("AddJob");
 let editJob = document.getElementById("editJob");
@@ -9,8 +9,42 @@ let btnSaveEdits = document.getElementById("btnSaveEdits");
 let btnCancelEdits = document.getElementById("btnCancelEdits");
 
 btnAddJobs.addEventListener("click", () => {
-	divAddjob.className = "hide";
+	divAddjob.className = "show";
 	editJob.className = "hide";
 });
-console.log(btnAddJobs,'click');
+// console.log(btnAddJobs,'click');
+btnNewJob.addEventListener("click",addNewJob); 
+async function addNewJob(){
+ const jobtitle = document.querySelector('input[name="jobtitle"]').value;
+ const company = document.querySelector('input[name="company"]').value;
+ const jobcategory = document.querySelector('input[name="jobcategory"]').value;
+ const region = document.querySelector('input[name="region"]').value;
+
+ const requestData = {
+    jobtitle,
+    company,    
+    jobcategory,
+    region,
+ };
+ await fetch("http://localhost:4000/jobs",{
+    method: 'POST',
+    headers:{
+        "content-type": "application/json",
+    },
+    body:JSON.stringify(requestData)
+ })
+}
+
+
+
+
+
+
+
+
+
 })();
+
+
+
+
